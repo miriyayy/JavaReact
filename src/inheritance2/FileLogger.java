6ec8817f474ae78efa9dd0783e9d3ce7d3268cba
miriyayy <mutlumiraysude34@gmail.com>
2024-10-16 09:43:17 +0300

    Java+React Camp

---
+++ src/inheritance2/FileLogger.java
@@ -0,0 +1,8 @@
+package inheritance2;
+
+public class FileLogger extends Logger {
+	
+	public void Log() {
+		System.out.println("Dosya Loglandı.");
+	}
+}
---
+++ src/inheritance2/Logger.java
@@ -0,0 +1,8 @@
+package inheritance2;
+
+public class Logger {
+
+	public void Log() {
+		System.out.println("Ortal konfigurasyon.");
+	}
+}
